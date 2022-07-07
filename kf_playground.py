9ab16868e753cd74d7d6b6b541307c6c21dde618
maijapietarila <maija.pietarila@fmi.fi>
2022-07-07 11:42:33 +0300

    add some files

---
+++ kf_playground.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+MAIJAN SOTKEMA KOPIO
+
+Basic Kalman filter code to minimize the cost function
+
+J = (x - xb)^T B-1 (x - xb) + (y - H(x))^T R-1 (y - H(x))
+
+x = state to be optimized
+xb = prior state
+B = prior state error covariance matrix (P-1 = inverse of P)
+y = observations
+H = observation operator
+R = observation error covariance matrix (R-1 = inverse of R)
+
+If H can be linealized and can be written in a matrix form, then H(x) = Hx.
+
+-------------------
+1) Use Kalman gain
+-------------------
+xa = xb + KG(y - Hxb) # Posterior state
+A = B - KHB           # Posterior state error covariance matrix
+KG = BHT(R + HBHT)-1  # Kalman gain
+HT = H^T              # Transpose of H
+
+-------------------
+2) Without Kalman gain
+-------------------
+xa = xb + (HTR-1H + B-1)-1 HTR-1 (y - Hxb) # Posterior state
+A = (HTR-1H + B-1)-1                       # Posterior state error covariance matrix
+
+--------------------
+Prediction 
+--------------------
+xb(t+1) = Mxa(t)  # state
+B (t+1) = MAMT + Q # covariance
+M = forecast model
+MT = M^T (transpose of M)
+Q = error covariance of the forecast model
+
+
+Author: @aki
+Project: 
+Rivision history
+File Created: 2022-06-23
+"""
+import numpy as np
+import xarray as xr
+from copy import deepcopy
+
+invm = np.linalg.inv # Function to invert matrix
+
+def initialize_state(nstate, x_mu, x_std, filename=None):
+    if filename is None: 
+        B = np.diag((np.ones(nstate)*x_std))
+        xb = np.random.normal(x_mu,x_std,size=nstate)
+    else: 
+        # Read data from file
+        data = xr.open_dataset(filename)
+        xb = data.state
+        B = data.cov
+        nstate = data.dims['nstate']
+    return xb, B, nstate
+
+def initialize_obs(nobs,y_mu,y_std,filename=None):
+    if filename is None:
+        t = np.arange(nobs) 
+        y = np.sin(t) + np.random.normal(loc=y_mu, scale=y_std, size=nobs)   
+        R = np.diag((np.ones(nobs)*y_std)) 
+    else:
+        # Read data from file
+        data = xr.open_dataset(filename)
+        y = data.obs
+        t = data.time
+        R = data.R
+        nobs = data.dims['nobs']
+    return t, y, R, nobs
+
+    
+def optimize(useKG, xb, B, y, H, R):
+    diff = y - H.dot(xb)
+    if useKG:
+        # Calculate Kalman gain
+        S  = R + H.dot(B).dot(H.T)
+        Si = invm(S)
+        KG = B.dot(H.T).dot(Si)
+
+        # Posteriors
+        xa = xb + KG.dot(diff)
+        A = B - KG.dot(H).dot(B)
+    else:
+        # Calcualte some matrices
+        Ri = invm(R)
+        Bi = invm(B)
+        S = (H.T).dot(Ri).dot(H) + Bi
+
+        # Posteriors
+        A = invm(S)        
+        xa = xb + A.dot(H.T).dot(Ri).dot(diff)
+
+    return xa, A, diff
+
+
+def predict(xa,A,M=None,Q=None):
+    if M is None:
+        xb = deepcopy(xa)
+        B  = deepcopy(A)
+    else:
+        xb = M.dot(xa)
+        B  = M.dot(A).dot(M.T) + Q
+    return xb, B
+
+
+def write_to_file(filename,xb,xa,B,A,time,y,R,diff):
+    out = xr.Dataset()
+    out['prior'] = (('nstate'),xb)
+    out['posterior'] = (('nstate'),xa)
+    out['prior_cov'] = (('nstate','nstate'), B)
+    out['posterior_cov'] = (('nstate','nstate'), A)
+    out['time'] = ('time',time)
+    out['obs'] = ('time',y)
+    out['obs_unc'] = (('time','time'),R)
+    out['differences'] = ('time',diff)
+    out.differences.attrs['comments'] = 'Obs - prior differences'
+    out.to_netcdf(filename)
+
+if __name__ == "__main__":
+
+    nstate = 20   # number of states
+    nobs = 500   # number of observations for the whole time
+    tw = 100      # length of time window
+    
+    x_mu = 1    # state mean values
+    y_mu = 1800 # observation mean values
+    x_std = 0.8 # state uncertainty
+    y_std = 15  # obs. uncertainty
+
+    # initialize values
+    xb, B, nstate = initialize_state(nstate, x_mu, x_std)
+    t, y, R, nobs = initialize_obs(nobs,y_mu,y_std)
+
+    useKG = True # whether to use Kalman gain or not
+    H = np.ones((nobs,nstate))*1800 # dummy observation operator
+
+    for timestep in range( int(len(t)/tw) ): #Loop through time
+        # Select observational data for this time 
+        # tuo w taitaa siis olla lista boolean-merkeistä että valkkaa aina tietyn aikaikkunan havainnot  y[w]
+        w = np.where((t>=timestep*tw)&(t<timestep*tw+tw))[0]
+        y_t = y[w]
+        R_t = R[w,:][:,w] 
+        H_t= H[w,:]
+
+        # Optimize
+        xa, A, diff = optimize(useKG, xb, B, y_t, H_t, R_t)
+
+        # Write data to netCDF file
+        wfile = f'output3/out_{timestep:02d}.nc'
+        write_to_file(wfile,xb,xa,B,A,t[w],y_t,R_t,diff)
+
+        # The updated state is prior for next state
+        xb, B = predict(xa,A)
+
+
