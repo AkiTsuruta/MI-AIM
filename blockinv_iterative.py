f49fd9960a351bc38236e530fd9ea642be421533
maijapietarila <maija.pietarila@fmi.fi>
2022-07-26 18:35:45 +0300

    some changes

--- blockinv_iterative.py
+++ blockinv_iterative.py
@@ -40,20 +40,79 @@ def block_inv(A,idx):
     return block([[E, F], [G, H]])
 
 
-def block_iter_inv(M,i):
+def block_iter_inv_fdi(M,i):
     """Function performs an iterative inversion of 2-D numpy array M in i>1 iterations by partitioning it
     into smaller blocks and inverting these one by one using a block Gaussian approach
     
-    WITH FOR-LOOP. Previously called block_iter_inv_f"""
+    WITH FOR-LOOP & default inv & individual assign"""
     n = len(M)
     k = int(n/i)
     l = k+k
-    E, F, G, H = M[0:k, 0:k], M[0:k, k:l], M[k:l, 0:k], M[k:l, k:l] 
+    E = M[0:k, 0:k]   
+    E = invm(E)
+    F = M[0:k, k:l]
+    G = M[k:l, 0:k]
+    H = M[k:l, k:l]
+    for m in range(l, (i+1)*k, k):
+        F = -E@F
+        H = invm(H+G@F)
+        G = -H@G@E
+        E = E+F@G
+        F = F@H
+        if m < (i-1)*k:
+            #Replace E with the inverted submatrix and form a block partition of a larger submatrix of M
+            E = block([[E, F], [G, H]])
+            F = M[0:m, m:m+k]
+            G = M[m:m+k, 0:m]
+            H = M[m:m+k, m:m+k] 
+        elif m == (i-1)*k: # Need to make sure that the last block partition doesn't "leave out" some cells as a result of rounding in k = int(n/i)
+            E = block([[E, F], [G, H]])
+            F = M[0:m, m:n]
+            G = M[m:n, 0:m]
+            H = M[m:n, m:n]    
+    return block([[E, F], [G, H]])
+
+def block_iter_inv_fdm(M,i):
+    """Function performs an iterative inversion of 2-D numpy array M in i>1 iterations by partitioning it
+    into smaller blocks and inverting these one by one using a block Gaussian approach
+    
+    WITH FOR-LOOP & default inv & multiple assign"""
+    n = len(M)
+    k = int(n/i)
+    l = k+k
+    E, F, G, H = M[0:k, 0:k], M[0:k, k:l], M[k:l, 0:k], M[k:l, k:l]
     E = invm(E)
     for m in range(l, (i+1)*k, k):
         F = -E@F
         H = H+G@F
-        G, H = G@E, invm(H)
+        H, G = invm(H), G@E
+        G = -H@G
+        # the line below can be parallelized if we can make sure
+        # both operations take their own copy of the value of F as it is
+        # before the line, and only after this F is overwritten by F@H
+        E, F = E+F@G, F@H
+        if m < (i-1)*k:
+            #Replace E with the inverted submatrix and form a block partition of a larger submatrix of M
+            E, F, G, H = block([[E, F], [G, H]]), M[0:m, m:m+k], M[m:m+k, 0:m], M[m:m+k, m:m+k] 
+        elif m == (i-1)*k: # Need to make sure that the last block partition doesn't "leave out" some cells as a result of rounding in k = int(n/i)
+            E, F, G, H = block([[E, F], [G, H]]), M[0:m, m:n], M[m:n, 0:m], M[m:n, m:n]    
+    return block([[E, F], [G, H]])
+
+def block_iter_inv_fbm(M,i):
+    """Function performs an iterative inversion of 2-D numpy array M in i>1 iterations by partitioning it
+    into smaller blocks and inverting these one by one using a block Gaussian approach
+    
+    WITH FOR-LOOP & block_inv & multiple assign"""
+    n = len(M)
+    k = int(n/i)
+    l = k+k
+    E, F, G, H = M[0:k, 0:k], M[0:k, k:l], M[k:l, 0:k], M[k:l, k:l]
+    idx = int(k/2)
+    E = block_inv(E, idx)
+    for m in range(l, (i+1)*k, k):
+        F = -E@F
+        H = H+G@F
+        H, G = block_inv(H, idx), G@E
         G = -H@G
         # the line below can be parallelized if we can make sure
         # both operations take their own copy of the value of F as it is
--- blockinv_iterative_symmetric.py
+++ blockinv_iterative_symmetric.py
@@ -11,38 +11,78 @@ Need to test if this is faster than the one that doesn't take symmetry into acco
 
 def block_inv_s(M, idx):
     n = len(M)
-    E, F, G, H = M[0:idx, 0:idx], M[0:idx, idx:n],  M[idx:n, 0:idx], M[idx:n, idx:n]  # E on nyt idx * idx -matriisi
+    E = M[0:idx, 0:idx]
     E = invm(E)
+    F = M[0:idx, idx:n]
+    G = M[idx:n, 0:idx]
+    H = M[idx:n, idx:n]  
     F = -E@F
-    H = H+G@F
-    H = invm(H)
+    H = invm(H+G@F)
     F = F@H
     E = E-F@G@E
     return block([[E, F], [transp(F), H]])
 
-
-def block_iter_inv_s(M, i):
-     """Function performs an iterative inversion of 2-D numpy array M in i>1 iterations by partitioning it
+def block_iter_inv_s(M,i):
+    """Function performs an iterative inversion of 2-D numpy array M in i>1 iterations by partitioning it
     into smaller blocks and inverting these one by one using a block Gaussian approach
     
-    Utilizes symmetry and for-loop"""
+    Utilizes symmetry & np.transpose"""
     n = len(M)
     k = int(n/i)
     l = k+k
-    E, F, G, H = M[0:k, 0:k], M[0:k, k:l], M[k:l, 0:k], M[k:l, k:l] 
-    E = block_inv_s(E, int(k/2))
+    E = M[0:k, 0:k]   
+    E = invm(E)
+    F = M[0:k, k:l]
+    G = M[k:l, 0:k]
+    H = M[k:l, k:l]
     for m in range(l, (i+1)*k, k):
         F = -E@F
-        H = H+G@F
-        G, H = G@E, block_inv_s(H, int(k/2))
-        G = -H@G
-        # the line below can be parallelized if we can make sure
-        # both operations take their own copy of the value of F as it is
-        # before the line, and only after this F is overwritten by F@H
-        E, F = E+F@G, F@H
+        H = invm(H+G@F)
+        F = F@H
+        E = E-F@G@E
         if m < (i-1)*k:
             #Replace E with the inverted submatrix and form a block partition of a larger submatrix of M
-            E, F, G, H = block([[E, F], [G, H]]), M[0:m, m:m+k], M[m:m+k, 0:m], M[m:m+k, m:m+k] 
+            E = block([[E, F], [transp(F), H]])
+            F = M[0:m, m:m+k]
+            G = M[m:m+k, 0:m]
+            H = M[m:m+k, m:m+k] 
         elif m == (i-1)*k: # Need to make sure that the last block partition doesn't "leave out" some cells as a result of rounding in k = int(n/i)
-            E, F, G, H = block([[E, F], [G, H]]), M[0:m, m:n], M[m:n, 0:m], M[m:n, m:n]    
+            E = block([[E, F], [transp(F), H]])
+            F = M[0:m, m:n]
+            G = M[m:n, 0:m]
+            H = M[m:n, m:n]    
+    return block([[E, F], [transp(F), H]])
+
+
+def block_iter_inv_s2(M,i):
+    """Function performs an iterative inversion of 2-D numpy array M in i>1 iterations by partitioning it
+    into smaller blocks and inverting these one by one using a block Gaussian approach
+    
+    Utilizes symmetry, doesn't compute np.transpose"""
+    n = len(M)
+    k = int(n/i)
+    l = k+k
+    E = M[0:k, 0:k]   
+    E = invm(E)
+    F = M[0:k, k:l]
+    G = M[k:l, 0:k]
+    H = M[k:l, k:l]
+    for m in range(l, (i+1)*k, k):
+        F = -E@F
+        H = invm(H+G@F)
+        G = -H@G@E
+        E = E+F@G
+        F = F@H
+        if m < (i-1)*k:
+            #Replace E with the inverted submatrix and form a block partition of a larger submatrix of M
+            E = block([[E, F], [G, H]])
+            F = M[0:m, m:m+k]
+            G = M[m:m+k, 0:m]
+            H = M[m:m+k, m:m+k] 
+        elif m == (i-1)*k: # Need to make sure that the last block partition doesn't "leave out" some cells as a result of rounding in k = int(n/i)
+            E = block([[E, F], [G, H]])
+            F = M[0:m, m:n]
+            G = M[m:n, 0:m]
+            H = M[m:n, m:n]    
     return block([[E, F], [G, H]])
+
