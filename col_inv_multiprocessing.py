ffc66dca9c82d8f4211f9da165c2ec842074e734
maijapietarila <maija.pietarila@fmi.fi>
2022-11-28 18:51:32 +0200

    developing a multiprocessing version of colinv

---
+++ col_inv_multiprocessing.py
@@ -0,0 +1,41 @@
+import xarray as xr
+import numpy as np
+import multiprocessing as mp
+import scipy.sparse.linalg
+
+gmres = scipy.sparse.linalg.gmres
+
+import multiprocessing as mp
+#as many processes as there are CPUs available
+
+nprocesses = mp.cpu_count()
+pool = mp.Pool(processes = nprocesses)
+
+def write_to_file(M, iM, exit_codes, filename):
+    out = xr.Dataset()
+    out["cov"] = (('nstate'), ('nstate'), M)
+    out["invcov"] = (('nstate'), ('nstate'), iM)
+    out["exit_codes"] = exit_codes
+    out.to_netcdf(filename)
+    
+
+
+def parallel_gmres(M, tol=1e-8):
+    n = len(M)
+    #this might be possible to be done with only creating each std unit vector
+    eye = np.eye(n)
+    icols_exitvals = pool.starmap(gmres, [(M, col, None, tol) for col in eye])
+
+    return icols_exitvals
+
+
+M = xr.open_dataset("regions_verify_202104_cov.nc")["covariance_bio"].values
+
+icols_exitvals = parallel_gmres(M)
+#split inverted columns and exits to two tuples
+icols, exits = zip(*icols_exitvals)
+#stack the columns into matrix form
+iM = np.stack(icols, axis = 1)
+
+write_to_file(M, iM, exits, "testinv1")
+
