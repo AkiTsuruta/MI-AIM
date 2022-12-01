4a4ea94968a1311d984596f2de0b8ce21c2bcfa6
maijapietarila <maija.pietarila@fmi.fi>
2022-12-01 19:05:44 +0200

    debugging

--- col_inv_multiprocessing.py
+++ col_inv_multiprocessing.py
@@ -3,11 +3,10 @@ import xarray as xr
 import numpy as np
 import multiprocessing as mp
 from scipy.sparse.linalg import gmres
+import time
 
-#as many processes as there are CPUs available
 
-nprocs = mp.cpu_count()
-pool = mp.Pool(processes=nprocs)
+
 
 def write_to_file(M, iM, exit_codes, filename):
     out = xr.Dataset()
@@ -19,6 +18,8 @@ def write_to_file(M, iM, exit_codes, filename):
 
 
 def parallel_gmres(M, tol=1e-8):
+    nprocs = mp.cpu_count()
+    pool = mp.Pool(processes=nprocs)
     n = len(M)
     #this might be possible to be done with only creating each std unit vector
     eye = np.eye(n)
@@ -27,13 +28,17 @@ def parallel_gmres(M, tol=1e-8):
     return icols_exitvals
 
 
-M = xr.open_dataset("data/regions_verify_202104_cov.nc")["covariance_bio"].values
+def main(filepath):
+    M = xr.open_dataset(filepath)["covariance_bio"].values
+    start = time.time()
+    icols_exitvals = parallel_gmres(M)
+    #split inverted columns and exits to two tuples
+    icols, exits = zip(*icols_exitvals)
+    #stack the columns into matrix form
+    iM = np.stack(icols, axis = 1)
+    end = time.time()
+    print(iM[0,:])
+    print(f"Inversion completed in {end-start} seconds")
+    #write_to_file(M, iM, exits, "testinv1")
 
-icols_exitvals = parallel_gmres(M)
-#split inverted columns and exits to two tuples
-icols, exits = zip(*icols_exitvals)
-#stack the columns into matrix form
-iM = np.stack(icols, axis = 1)
-
-write_to_file(M, iM, exits, "testinv1")
-
+main("data/regions_verify_isotope_202112_cov.nc")
