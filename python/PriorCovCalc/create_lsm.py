fe228f09514a3dcd92c8347c13aeb696124fc0de
maijapietarila <maija.pietarila@fmi.fi>
2024-05-31 16:56:34 +0300

    add possibility to write out lsm coords

--- python/PriorCovCalc/create_lsm.py
+++ python/PriorCovCalc/create_lsm.py
@@ -5,23 +5,38 @@ import numpy as np
 from global_land_mask import globe
 import os
 
+
 def create_lsm(latmin, latmax, lonmin, lonmax):
      """Creates a land-sea mask (land=1, sea=0) in 0.1 deg x 0.1 deg resolution
      for the chosen area.
      """
-     lat = np.linspace(latmin,latmax, (latmax-latmin)*10+1, dtype=np.float32)
-     lon = np.linspace(lonmin,lonmax, (lonmax - lonmin)*10+1, dtype=np.float32)
-     print("creating longrid, latgrid")
+     lat = np.linspace(latmin,latmax, (latmax-latmin)*10+1)#, dtype=np.float32)
+     lon = np.linspace(lonmin,lonmax, (lonmax - lonmin)*10+1)#, dtype=np.float32)
      longrid, latgrid = np.meshgrid(lon,lat)
-     print("computing lsm")
      lsm = globe.is_land(latgrid, longrid)
    
      return lsm, latgrid, longrid
 
+def pick_coords(lsm, latgrid, longrid, v):
+    if v == "land":
+        latv = latgrid[lsm].flatten()
+        lonv = longrid[lsm].flatten()
+        
+                         
+    elif v == "ocean":
+        latv = latgrid[~lsm].flatten()
+        lonv = longrid[~lsm].flatten()
+    
+    return latv, lonv    
 
+
+def write_coords_to_file(latv, lonv, v):
+    coords = np.column_stack(lonv, latv)
+
+    
 #output 
-#OUTPUT_PATH = '/home/pietaril/Documents/data/unc_cov_matrices'
-OUTPUT_PATH = '/scratch/project_462000459/maija/data/co2_prior_unc_cov'
+OUTPUT_PATH = '/home/pietaril/Documents/data/coords'
+#OUTPUT_PATH = '/scratch/project_462000459/maija/data/co2_prior_unc_cov'
 
 
 # latmin = 30.0
@@ -29,16 +44,23 @@ OUTPUT_PATH = '/scratch/project_462000459/maija/data/co2_prior_unc_cov'
 # lonmin = -10.0
 # lonmax = 0.0
 
-latmin = 30.0
-latmax = 75.0
-lonmin = -15.0
-lonmax = 40.0
-
-fname = f"lsm_lat{latmin}_{latmax}_lon{lonmin}_{lonmax}"
+#Europe
+latmin = 30
+latmax = 75
+lonmin = -15
+lonmax = 40
 
 
 lsm, latgrid, longrid = create_lsm(latmin, latmax, lonmin, lonmax)
 
-for arr in [lsm, latgrid, longrid]:
-     np.save(os.join(OUTPUT_PATH))
+for v in ["land", "ocean"]:
+    latv, lonv = pick_coords(lsm, latgrid, longrid, v)
+    print(len(latv))
+    print(len(lonv))
+    out = xr.Dataset(
+        data_vars={"lon": (lonv), "lat": (latv),},
+        attrs={'comment': f"Coordinates for {v} cells on a 0.1 x 0.1 deg grid over Europe"})
+    fname = f"{v}_coords.nc"
+    out.to_netcdf(os.path.join(OUTPUT_PATH, fname))
+    
 
