8aead53def43b6ee8b5f6def0b0193170854b7a4
maijapietarila <maija.pietarila@fmi.fi>
2024-06-27 17:59:30 +0300

    clean up

--- julia/ComputePriorCovCO2.jl
+++ julia/ComputePriorCovCO2.jl
@@ -4,14 +4,13 @@ include("/home/pietaril/Documents/MI-AIM/julia/KLHelpers.jl")
 #include("/project/project_462000459/maija/kl_inv/KLHelpers.jl")
 
 using NCDatasets
-using Distances
+#using Distances
 
 
-function write_cov_to_file(K::Matrix{T}, x::Array{T,2}, v::String, outpath::String, filename::String) where T <: Real
+function write_cov_to_file(K::Matrix{T}, x::Array{T,2}, fname::String, v::String, datapath::String) where T <: Real
     #For saving the cov matrix and coordinates as a netcdf file
     n = size(K, 1);
-
-    out = NCDataset("$(outpath)/$(filename).nc", "c");
+    out = NCDataset(datapath*fname, "c");
     defDim(out, "nstate", n);
     variab = defVar(out, "covariance", Float64, ("nstate", "nstate"));
     variab[:,:] = K;
@@ -21,9 +20,34 @@ function write_cov_to_file(K::Matrix{T}, x::Array{T,2}, v::String, outpath::Stri
     lat[:] = x[2,:];
     lon[:] = x[1,:];
     close(out);
-    
-end 
 
+end
+
+
+function main(v::String, datapath::String, area::String)
+    println("Reading in coordinates");
+    x = KLHelpers.read_coords_from_file(datapath*"coords/$(area)_$(v)_coords.nc");
+    # Uncertainty (std)
+    sigmas = Dict([("land", 0.8),("ocean", 1.2)]);
+
+
+    # Correlation length (km)
+    L = Dict([("land", 100), ("ocean", 500)]);
+
+    println("Computing cov matrix")
+    K = KLHelpers.expker(x, sigmas[v], L[v]);
+
+    fname= "prior_co2_unc_$(area)_$(v).nc";
+    println("Writing to file")
+    write_cov_to_file(K, x, fname, v, datapath);
+    println("Done")
+
+end
+
+
+#datapath="/scratch/project_462000459/maija/data/co2_prior_unc_cov/";
+datapath = "/home/pietaril/Documents/data/co2_prior_unc_cov/";
+#main("ocean", datapath, "wholeur")
 
 
 
@@ -33,22 +57,9 @@ v = "land";
 pathtocoords = "/home/pietaril/Documents/data/co2_prior_unc_cov/coords/centeur_$(v)_coords.nc";
 
 
-x = KLHelpers.read_coords_from_file(pathtocoords);
-x = x[:,1:100];
 
+#outpath = "/home/pietaril/Documents/data/co2_prior_unc_cov"
+#filename="memtest1";
 
-
-# Uncertainty (std)
-sigmas = Dict([("land", 0.8),("ocean", 1.2)]); 
-
-
-# Correlation length (km)
-L = Dict([("land", 100), ("ocean", 500)]);
-
-K = KLHelpers.expker(x, sigmas[v], L[v]);
-
-outpath = "/home/pietaril/Documents/data/co2_prior_unc_cov"
-filename="testcov_$v";
-
-#write_cov_to_file(K, x, v, outpath, filename);
+#@time write_cov_to_file(K, x, v, outpath, filename);
 
