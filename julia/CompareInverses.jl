8281261cd86b0174d19cb5208b319d065105be92
maijapietarila <maija.pietarila@fmi.fi>
2024-06-28 17:34:53 +0300

    getting stats out

--- julia/CompareInverses.jl
+++ julia/CompareInverses.jl
@@ -2,20 +2,63 @@
 include("/home/pietaril/Documents/MI-AIM/julia/KLHelpers.jl")
 
 using LinearAlgebra
-using NCDatasets
+using DataFrames
 using Plots
+using CSV
+using Statistics
+
+v = "ocean";
+rho = 2;
+n_neighbors = 9;
 
 datapath = "/home/pietaril/Documents/data/co2_prior_unc_cov/inv/";
 fname_py = "pyinv_test_land.nc";
 fname_kl = "testinv_land_8_10.nc";
 
-iK_kl = KLHelpers.read_iK_from_file(datapath*fname_kl)
-iK_py = KLHelpers.read_iK_from_file(datapath*fname_py)
-difference = iK_py - iK_kl
-norm(difference)
-opnorm(difference)
-maximum(difference)
 
-colormap = cgrad(:vik);
-p2 = heatmap(difference, c = colormap, yflip = true)
+
+function compare_invs(vs::AbstractVector{String}, rhos::AbstractVector{<:Integer}, neighborss::AbstractVector{<:Integer}) 
+    # Tests for timing Cholesky of Matern32 with l = [0.001]. Need to adjust this later to take also k and l as inputs so that they can also vary.
+    # For some reason passing a function k didn't work
+    res = DataFrame()
+    i = 0
+    for v in vs
+        iK_py = KLHelpers.read_iK_from_file(datapath*"pyinv_test_land.nc");
+        #pre-allocate diffr matrix
+        diffr = zeros(size(iK_py));
+        for rho in rhos
+            for neighbors in neighborss
+                    i += 1
+                    #read kl-inverted matrix into the difference matrix
+                    diffr .= KLHelpers.read_iK_from_file(datapath*"KLinv_centeur_$(v)_$(rho)_$(neighbors).nc");
+
+                    # in-place subtraction
+                    diffr .= iK_py .- diffr;
+
+                    frob = norm(diffr);
+                    frob_rel = frob / norm(iK_py);
+                    maxerr = maximum(abs, diffr);
+                    qs = quantile(diffr[:], [0.1, 0.25, 0.5, 0.75, 0.9])
+
+                    
+                    push!(res, (id = i, N = size(diffr)[1], v = v, rho = rho, n_neighbors = neighbors,
+                        frobenius = frob, relative_frobenius = frob_rel, maxerr=maxerr, quantiles=qs))
+                
+            end
+        end
+    end
+    return res
+
+end
+
+res = compare_invs(["land"], [2], [9])
+
+norm(diffr)
+maximum(abs, diffr)
+
+
+
+#the colorbar doesn't center at zero for some reason
+#colormap = cgrad(:vik);
+#p2 = heatmap(difference, c = colormap, yflip = true)
 
